224e4262536722c9fe348e1a05c6f1ca06cdbf68
Ajinkya Tambade <ajinkyatambade143@gmail.com>
2024-09-05 12:45:11 +0530

    Javascript basics

---
+++ JavaScript/Login.js
@@ -0,0 +1,12 @@
+function handleSubmit() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if (username === "" || password === "") {
+        document.getElementById("error-message").innerHTML = "Both fields are required.";
+    } else {
+        document.getElementById("error-message").innerHTML = ""; // Clear error message
+        console.log("Username: " + username);
+        console.log("Password: " + password);
+    }
+}
